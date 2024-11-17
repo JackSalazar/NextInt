466cd33bacb8531b4ed291c6147062afb422660c
Jack Salazar <jack.salazar.649@my.csun.edu>
2024-11-16 19:57:28 -0800

    got in_range to work

--- mips/nextInt.s
+++ mips/nextInt.s
@@ -7,6 +7,8 @@ buff:   .space 256                        #static char [] buffer = new char[256]
         .include "include/subroutine.s"
 
 
+
+
 nextInt: nop                        #public static int nextInt (int radix) { // you are going from base radix to base 10
         #bookkeeping
         #a0: radix                #
@@ -28,41 +30,55 @@ nextInt: nop                        #public static int nextInt (int radix) { // 
 
 
         lb $a0, 0($t6)
-        call glyph2int $a0, $t7     #PRAY THIS WORKSSSSSSSSSSSSSSSSS          #    digit = glyph2int(buffer[0], radix);
-        move $t1, $v0       #this moves the result to digit, which has to be split from the above equation^
+        #call glyph2int $a0, $t7     #PRAY THIS WORKSSSSSSSSSSSSSSSSS          #    digit = glyph2int(buffer[0], radix);
+        #move $t1, $v0       #this moves the result to digit, which has to be split from the above equation^
 
-        add $t0, $zero, $zero                #   number=0
-loop:        beq $t1, '-1', done                #    for(; digit != -1 ;) {
+                #start of testing in_range
+                li $t4, '0' #min
+                li $t5, '9'#max
+                move $t3, $a0
+        call in_range $t3, $t4, $t5 #this calls the function
+        move $a0, $v0 #this moves the true/false to the staging area
+        li $v0, 1 #1 is to call int located at $a0
+        syscall #execute the call
+        move $v0, $zero
+        jr $ra #end the function
+
+                #end testing of in_range
+
+        #add $t0, $zero, $zero                #   number=0
+loop:   #beq $t1, '-1', done                #    for(; digit != -1 ;) {
 body: nop                        #      //System.out.println("Buffer "+ i +" is "+ buffer[i]);
                         #      //number = (number * radix) + digit ; 
-        mult $t0, $t7                 #      number = number * radix;
-        mflo $t0
-        add $t0, $t0, $t1                #      number = number + digit;
-        call glyph2int $a0, $t7                #      digit = glyph2int(buffer[i], radix);
-        move $t1, $v0   #this moves the result to digit, which has to be split from the above equation^
-next:   nop
-        addi $t2, $t2, 1                #      i = i + 1;
-        lb $a0, $t2($t6)
+        #mult $t0, $t7                 #      number = number * radix;
+        #mflo $t0
+        #add $t0, $t0, $t1                #      number = number + digit;
+        #call glyph2int $a0, $t7                #      digit = glyph2int(buffer[i], radix);
+        #move $t1, $v0   #this moves the result to digit, which has to be split from the above equation^
+next:   #nop
+        #addi $t2, $t2, 1                #      i = i + 1;
+        #lb $a0, $t2($t6)
                         #      //System.out.println("number is" + number);
                         #    }
 done:                        
                         #    //System.out.println("number is" + number);
-                        #    return number;
-                        #}
+        #move $v0, $t0                    #    return number;
+        #jr $ra                    #}
                         #
 glyph2int: nop                        #public static int glyph2int(char c, int radix) {
         #bookkeeping
-        #t3: c, input
-        #a0: radix                #
-        add $v0, $zero, $t7 
-                        #    char input = c;
-                        #    int digit; 
+        #a0: c, input
+        #t1: digit
+        #t7: radix                #
+        
+        #redundant                #    char input = c;
+        #also redundant                #    int digit; 
                         #
                         #    //mips.read_c();
                         #    //input = mips.retval();
                         #
                         #
-                        #    if (in_range(input, '0', '9')) {//0' <= input && input <= '9') {
+        #call in_range                 #    if (in_range(input, '0', '9')) {//0' <= input && input <= '9') {
                         #      digit = input - '0';
                         #    }
                         #    else if ( in_range(input, 'A', 'Z')) { //A' <= input && input <= 'Z') {
@@ -84,15 +100,17 @@ glyph2int: nop                        #public static int glyph2int(char c, int r
                         #    return digit;
                         #}
                         #
-in_range: nop                        #public static boolean in_range(char value, char min, char max){
+in_range: nop      #CONFIRMED WORKING. DON'T TOUCH                  #public static boolean in_range(char value, char min, char max){
         #bookkeeping
         #t3: value
         #t4: min
-        #t5: max                
-                        #    if (value >= min){
-                        #        if (value <= max){
-                        #            return true;
-                        #        }
+        #t5: max 
+        
+
+fcomp:  blt $t3, $t4, ir_done                       #    if (value >= min){
+scomp:  bgt $t3, $t5, ir_done                      #        if (value <= max){
+        addi $v0, $zero, 1              #            return true;
+        jr $ra                #        }
                         #    }
-                        #    return false;
-                        #}
+ir_done:add $v0, $zero, $zero                      #    return false;
+        jr $ra                #}
